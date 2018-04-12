03072f13229156cedd7a7f0fd7b4217988f84112
haarlike <wl@wldeMacBook-Pro.local>
2018-04-12 21:37:46 +0800

    http client

--- app/controllers/MessageAppController.php
+++ app/controllers/MessageAppController.php
@@ -9,6 +9,39 @@ class MessageAppController extends BackstageController
     );
 
     function testAction(){
+        $start =time();
 
+        $request_list ;
+        for($i=0 ;$i<60;$i++){
+            $request_list[] =array(
+                'url'=>'http://mmgo.com',
+            );            
+        }
+
+        foreach($request_list as $v){
+            \Utils::curl_client($v['url']);
+        }
+
+        $end =time();
+        echo $end-$start ;
+        exit;
+    }
+
+    function test2Action(){
+        $start =time();
+
+        $request_list =[];
+        for($i=0 ;$i<60;$i++){
+            $request_list[] =array(
+                'url'=>'http://mmgo.com',
+            );            
+        }
+        $httpClient =new \HttpClient();
+        $res =$httpClient ->multiple($request_list);
+        //        var_dump($res);
+        //var_dump($httpClient->getErrors());
+
+        $end =time();
+        echo $end-$start;
     }
 }
---
+++ app/library/HttpClient.php
@@ -0,0 +1,71 @@
+<?php
+use GuzzleHttp\Pool;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+class HttpClient{
+
+    private $method;
+
+    private $headers=[];
+
+    private $timeout=60;
+
+    private $errors=[];
+
+    private $return=[];
+
+    public function getErrors(){
+        return $this->errors;
+    }
+
+    public function multiple($request_list ,$method='GET' ,$timeout=20 ,$headers=array() ){
+
+        $this->method =strtoupper($method);
+        $this->headers =$headers;
+        $this->timeout =$timeout;
+
+        $client = new Client(array(
+            'timeout'  => $timeout
+        ));
+
+        $pool = new Pool($client, $this->request($request_list), [
+            'concurrency' => count($request_list),
+            'fulfilled' => function ($response, $index) {
+                $this->return[]=array(
+                    'index'=>$index,
+                    'content'=>$response->getBody()->getContents()
+                );
+            },
+            'rejected' => function ($reason, $index) {
+                $this->errors[]=array(
+                    'index'=>$index,
+                    'reason'=>$reason->getMessage()
+                );
+            },
+        ]);
+
+        $promise = $pool->promise();
+        $promise->wait();
+        return $this->return;
+    }
+
+    /*
+    eg:  headers 
+    array(
+        'Content-Type' =>'application/json; charset=utf-8',
+        'Content-Length' =>strlen($data),
+    )
+    */
+    public function request($request_list ){
+        foreach ($request_list as $request) {
+            $url =$request['url'];
+            yield new Request(
+                $this->method,
+                $url,
+                $this->headers,
+                $request['data']
+            );
+        }
+    }
+}
