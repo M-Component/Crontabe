43f71d1b6bb9a42588e748253708fa890025b93e
Medivh <maidivh@163.com>
2018-04-17 15:12:46 +0800

    bug fix

--- app/library/Api/Goods.php
+++ app/library/Api/Goods.php
@@ -9,8 +9,8 @@ class Goods extends Base
     {
         $data['limit'] = $this->request->get('limit') ?: 10;;
         $data['page'] = $this->request->get('page') ?: 1;
-
         $data['keywords'] = $this->request->get('keywords');
+        $data['store_name'] = $this->request->get('store_name');
         $goods = \Utils::curl_client($this->config."/api/search/goods", $data);
         $this->response->setHeader('Content-Type', 'application/json');
         $this->response->setContent($goods);
