a30ccbfec73622e031d60becc84a1eead6c6600e
Medivh <maidivh@163.com>
2018-04-17 14:56:30 +0800

    bug fix

--- app/library/Api/Goods.php
+++ app/library/Api/Goods.php
@@ -2,27 +2,28 @@
 namespace Api;
 class Goods extends Base
 {
-    public function getList(){
-        $data['limit'] = $this->request->get('limit') ? :10;;
-        $data['page'] = $this->request->get('page') ? :1;
+    public function getList()
+    {
+        $data['limit'] = $this->request->get('limit') ?: 10;;
+        $data['page'] = $this->request->get('page') ?: 1;
         $data['keywords'] = $this->request->get('keywords');
-        $goods = \Utils::curl_client("http://120.132.13.132/api/search/goods",$data);
-        $res = json_decode($goods,1);
-        if (!$res['errorCode']){
-            $this->success($goods);
-        }
-        $this->error($goods['msg']);
+        $goods = \Utils::curl_client("http://120.132.13.132/api/search/goods", $data);
+        $this->response->setHeader('Content-Type', 'application/json');
+        $this->response->setContent($goods);
+        $this->response->send();
+        exit;
+
     }
-    
-    public function getGoodsRow($id){
-        if (!$id){
+
+    public function getGoodsRow($id)
+    {
+        if (!$id) {
             $this->getList();
         }
-        $goods = \Utils::curl_client("http://120.132.13.132/api/search/goods/".$id);
-        $res = json_decode($goods,1);
-        if (!$res['errorCode']){
-            $this->success($goods);
-        }
-        $this->error($goods['msg']);
+        $goods = \Utils::curl_client("http://120.132.13.132/api/search/goods/" . $id);
+        $this->response->setHeader('Content-Type', 'application/json');
+        $this->response->setContent($goods);
+        $this->response->send();
+        exit;
     }
 }
