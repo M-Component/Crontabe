90e8e2cc5ac103f1b20b105f07b7d911bcbfb533
haarlike <wl@wldeMacBook-Pro.local>
2018-04-20 18:06:14 +0800

    bug fix

--- app/controllers/MessageAppController.php
+++ app/controllers/MessageAppController.php
@@ -7,4 +7,16 @@ class MessageAppController extends BackstageController
     var $custom_action = array(
         'use_add' => false
     );
+    public function jpushAction(){
+        $sender = new \Sender\App();
+        $targets =array(
+            'target'=>'1a0018970a8b3045795'
+        );
+        $res =$sender->sendOne($target , '' ,'' ,array(
+            'message' =>'hello world !'
+        ));
+        var_dump($res);
+        exit;
+
+    }
 }
